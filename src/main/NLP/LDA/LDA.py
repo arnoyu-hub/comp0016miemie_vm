0d5459123703eb6e721fcaf93405c862881dc1cb
Arno <72074041+arnoyu-hub@users.noreply.github.com>
2022-04-08 14:22:27 +0100

    Update LDA.py

--- src/main/NLP/LDA/LDA.py
+++ src/main/NLP/LDA/LDA.py
@@ -411,11 +411,12 @@ class Lda():
         """
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
         # map description to a document-count matrix.
+        COUNT = 0
         try:
            X = self.vectorizer.fit_transform(self.data['Module_Description'])
         except KeyError as e:
-            pass
-        if (e):
+            COUNT += 1
+        if (COUNT == 1):
             X = self.vectorizer.fit_transform(self.data['Description'])
         corpus = gensim.matutils.Sparse2Corpus(X, documents_columns=False) # transform sparse matrix as a gensim corpus. 
         id2word = dict((v, k) for k, v in self.vectorizer.vocabulary_.items())
